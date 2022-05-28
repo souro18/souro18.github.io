78c75c6a3ac3a7cd29351ca7f9aede55d70691c3
sourojit <sourojit@sb-tech-045.local>
2022-05-28 19:51:00 +0530

    Updates

---
+++ precache-manifest.d50ceb0420f9f464c855f5cad03f7e12.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2456dc9507199f933469616157693d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b2d9b0298c4953c46c",
+    "url": "/static/css/3.2d83a72d.chunk.css"
+  },
+  {
+    "revision": "7066a38748b5bb02f9a7",
+    "url": "/static/css/4.8c6a30d8.chunk.css"
+  },
+  {
+    "revision": "6670117f930e01942e32",
+    "url": "/static/css/5.a24a6583.chunk.css"
+  },
+  {
+    "revision": "3f60521ec5042d8550db",
+    "url": "/static/css/6.6301dfd1.chunk.css"
+  },
+  {
+    "revision": "731968a276744c0a0124",
+    "url": "/static/css/main.c0b844e8.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b2d9b0298c4953c46c",
+    "url": "/static/js/3.97df2bd2.chunk.js"
+  },
+  {
+    "revision": "7066a38748b5bb02f9a7",
+    "url": "/static/js/4.cd94f742.chunk.js"
+  },
+  {
+    "revision": "6670117f930e01942e32",
+    "url": "/static/js/5.a6a732e5.chunk.js"
+  },
+  {
+    "revision": "3f60521ec5042d8550db",
+    "url": "/static/js/6.e017bb02.chunk.js"
+  },
+  {
+    "revision": "731968a276744c0a0124",
+    "url": "/static/js/main.8290d503.chunk.js"
+  },
+  {
+    "revision": "dffa593cbf0e79f1b2f8",
+    "url": "/static/js/runtime-main.4b7d7755.js"
+  },
+  {
+    "revision": "f00cf82578b2474232fe1f049081894b",
+    "url": "/static/media/Sourajit_Paul_CV.f00cf825.pdf"
+  },
+  {
+    "revision": "b361cd253d23c62348ab48bb4ae10f5c",
+    "url": "/static/media/coder-shoot.b361cd25.jpg"
+  },
+  {
+    "revision": "1c37180f4e2d9e5586478224e5b63940",
+    "url": "/static/media/self.1c37180f.jpg"
+  }
+]);
